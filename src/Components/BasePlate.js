a7732c334ace8920452facc65364df4160359975
Albert Arrieta <albert.arrieta12@gmail.com>
2020-09-13 10:39:49 -0600

    Initial commit. +Header +Baseplate components

---
+++ src/Components/BasePlate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BasePlate(){
+    return(
+        <div className="text-center">
+            <h1 className="text-6xl">3D-Printer Fillament Calculator</h1>
+        </div>
+    )
+}
+
+export default BasePlate;
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="text-center">
+      <h1 className="text-6xl">3D-Printer Fillament Calculator</h1>
+    </div>
+  );
+}
+
+
+export default Header;
